23afdc26e04b83c618a748d53ce09390d7d58892
Sbastdia <91331243+Sbastdia@users.noreply.github.com>
2022-11-20 20:05:25 +0000

    aa

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'user',
+        'USER': 'postgres',
+        'PASSWORD': 'mipostgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
